60974999221236eec1ed5655337fb6e931f36f0c
musakayretli <musakayretli06@gmail.com>
2022-10-11 20:38:52 +0300

    dffdgdf

---
+++ src/MainApp.java
@@ -0,0 +1,12 @@
+/**
+ * başlangıç sınıfıdır
+ * musa kayretli
+ */
+public class MainApp {
+    public static void main(String[] args) {
+        System.out.println("merhaba...");
+        System.out.println("dünya");
+
+
+    }
+}
